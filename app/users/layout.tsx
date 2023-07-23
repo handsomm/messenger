2051780a9ef84dc49bfc7beb23537b5b1769028f
Soumya <soumyaranjanandcbu@gmail.com>
2023-07-23 11:42:39 +0530

    Created the people page

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Sidebar from '../components/sidebar/Sidebar'
+import getUser from '../actions/getUsers'
+import getUsers from '../actions/getUsers'
+import UserList from './components/UserList'
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
+    const users = await getUsers()
     return (
         <Sidebar>
             <div className='h-full'>
+                <UserList items={users} />
                 {children}
             </div>
         </Sidebar>
